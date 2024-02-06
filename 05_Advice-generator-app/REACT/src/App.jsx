9e395743e0949d48b1bff3ad8a082e41e442a10b
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-02-07 00:48:09 +0530

    completed

---
+++ 05_Advice-generator-app/REACT/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import patternDividerM from "./assets/images/pattern-divider-mobile.svg";
+import patternDividerD from "./assets/images/pattern-divider-desktop.svg";
+import iconDice from "./assets/images/icon-dice.svg";
+import { useEffect } from "react";
+
+function App() {
+  const [advice, setAdvice] = useState("");
+  const [adviceId, setAdviceId] = useState("");
+  const fetchAdvice = () => {
+    fetch("	https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        // console.log(data.slip.advice)
+        // console.log(data.slip.id)
+        setAdvice(data.slip.advice);
+        setAdviceId(data.slip.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  const handleDiseThrow = () => {
+    fetchAdvice();
+  };
+  return (
+    <>
+      <div className="main-container">
+        <div className="container">
+          <div className="advice-section">
+            <p className="advice-id">ADVICE #{adviceId}</p>
+            <p className="advice-quote">"{advice}"</p>
+
+            <img className="divider-m" src={patternDividerM} alt="divider" />
+            <img className="divider-d"src={patternDividerD} alt="divider" />
+          </div>
+
+          <div className="dise-container" onClick={handleDiseThrow}>
+            <img src={iconDice} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
